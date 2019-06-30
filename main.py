59a4d4fb8aa54c18b4c4d63871fd90cc4dd27859
stapox35 <stapox35@gmail.com>
2019-06-30 23:02:33 +0200

    Kolejne poprawki, dodane layouty. TODO: Copyrajta na srodek i buttony na białe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMessageBox, QHBoxLayout, QVBoxLayout
 from ksztalty import Ksztalty, Ksztalt
 # from PyQt5.QtWidgets import QHBoxLayout2
 from PyQt5.QtGui import QPainter, QColor, QPolygon, QPixmap, QIcon, QImage
@@ -6,7 +6,7 @@ from PyQt5.QtCore import QPoint, QRect, QSize
 from urllib import *
 from urllib.request import urlopen
 
-flagamiejsca = 0
+globalURL= "http://stapox.cal24.pl/"
 
 
 class menedzer(QWidget):
@@ -17,79 +17,106 @@ class menedzer(QWidget):
 
     def interfejs(self):
         
-        if flagamiejsca == 0:
+
+       
+
+        layoutV = QVBoxLayout()
+        layoutV.setDirection(2)
+
+       
+        layout = QHBoxLayout()
+        
+        label = QLabel(self)
+        url = globalURL+"img/logo_maszyna.gif"  
+        data = urlopen(url).read()
+        pixmap = QPixmap()
+        pixmap.loadFromData(data)
+        pixmap2 = pixmap.scaled(251, 70)
+        label.setPixmap(pixmap2)
+        label.move(20,15)
+        layout.addWidget(label)
+
+       
+        napis = QLabel(self)
+        napis.setText("Menedżer nieoficjalnych dodatków")
+        napis.setStyleSheet("font: 30pt Times New Roman; color: white; font-weight: 700")
+        napis.move(400, 25)
 
 
-            self.gorna_belka = Ksztalt(self, Ksztalty.Rect)
-            self.gorna_belka.ustawSize(x=-1, y=-1, xsize=1205, ysize=125)
-            self.gorna_belka.ustawkolorO(255,255,255)
-            self.gorna_belka.ustawgradient(1200,100,1200,125,0,126,67,255,255,255)
+       
 
 
-            self.dolna_belka = Ksztalt(self,Ksztalty.Rect)
-            self.dolna_belka.ustawSize(x=-1, y=551, xsize=1202, ysize=125)
-            self.dolna_belka.ustawgradient(1200,575,1200,550,0,126,67,255,255,255)
-            self.dolna_belka.ustawkolorO(255,255,255)
 
-            
-            label = QLabel(self)
-            url = "http://stapox.cal24.pl/img/logo_maszyna.gif"  
-            data = urlopen(url).read()
-            pixmap = QPixmap()
-            pixmap.loadFromData(data)
-            pixmap2 = pixmap.scaled(251, 70)
-            label.setPixmap(pixmap2)
-            label.move(20,15)
+        layout.addWidget(napis)
+        layoutV.addLayout(layout)
 
+        layout2 = QHBoxLayout()
 
-            napis = QLabel(self)
-            napis.setText("Menedżer nieoficjalnych dodatków")
-            napis.setStyleSheet("font: 30pt Times New Roman; color: white; font-weight: 700")
-            napis.move(400, 25)
+        zdjecie = QLabel(self)
+        url2 = globalURL+"img/img_tytulowa.jpg"
+        data =  urlopen(url2).read()
+        pixmap3 = QPixmap()
+        pixmap3.loadFromData(data)
+        #  pixmap4 = pixmap3.scaled(755, 425)
+        zdjecie.setPixmap(pixmap3)
+        zdjecie.move(0,125)
+        
 
-            napis2 = QLabel(self)
-            napis2.setText("Copyright © 2019 stapox ")
-            napis2.setStyleSheet("font: 30pt Times New Roman; color: white")
-            napis2.move(400, 600)
+        layout2.addWidget(zdjecie)
+        layout3 = QHBoxLayout()
+        layout3.setDirection(2)
+        dodajBtn = QPushButton("&Instaluj dodatki", self)
+        dodajBtn.setStyleSheet("width: 200px; height: 75px")
+        layout3.addWidget(dodajBtn)
+        dodajBtn2 = QPushButton("O &projekcie", self)
+        dodajBtn2.setStyleSheet("width: 200px; height: 75px")
+        layout3.addWidget(dodajBtn2)
+        dodajBtn3 = QPushButton("O &zespole", self)
+        dodajBtn3.setStyleSheet("width: 200px; height: 75px")
+        layout3.addWidget(dodajBtn3)
+        dodajBtn4 = QPushButton("&Kontakt", self)
+        dodajBtn4.setStyleSheet("width: 200px; height: 75px")
+        layout3.addWidget(dodajBtn4)
+        layout2.addLayout(layout3)
+        layoutV.addLayout(layout2)
+        napis2 = QLabel(self)
+        napis2.setText("Copyright © 2019 stapox ")
+        napis2.setStyleSheet("font: 30pt Times New Roman; color: white; text-align: center")
+        layoutpomocniczy = QHBoxLayout()
+        layoutpomocniczy.addWidget(napis2)
+        #layoutpomocniczy.addSpacing(50)
+        layoutV.addLayout(layoutpomocniczy)
 
+       
 
-            zdjecie = QLabel(self)
-            url2 = "http://stapox.cal24.pl/img/img_tytulowa.jpg"
-            data =  urlopen(url2).read()
-            pixmap3 = QPixmap()
-            pixmap3.loadFromData(data)
-            #  pixmap4 = pixmap3.scaled(755, 425)
-            zdjecie.setPixmap(pixmap3)
-            zdjecie.move(0,125)
+        
+        dodajBtn.clicked.connect(self.dzialanie)
+        dodajBtn2.clicked.connect(self.dzialanie)
+        dodajBtn3.clicked.connect(self.dzialanie)
+        dodajBtn4.clicked.connect(self.dzialanie)
 
-            dodajBtn = QPushButton("&Instaluj dodatki", self)
-            dodajBtn.setStyleSheet("width: 200px; height: 75px")
-            dodajBtn.move(900, 145)
+        layoutX = QVBoxLayout()
+        layoutX.setDirection(2)
 
-            dodajBtn2 = QPushButton("O &projekcie", self)
-            dodajBtn2.setStyleSheet("width: 200px; height: 75px")
-            dodajBtn2.move(900, 245)
-
-            dodajBtn3 = QPushButton("O &zespole", self)
-            dodajBtn3.setStyleSheet("width: 200px; height: 75px")
-            dodajBtn3.move(900, 345)
-
-            dodajBtn4 = QPushButton("&Kontakt", self)
-            dodajBtn4.setStyleSheet("width: 200px; height: 75px")
-            dodajBtn4.move(900, 445)
-
-
-           
-            dodajBtn.clicked.connect(self.dzialanie)
-            dodajBtn2.clicked.connect(self.dzialanie)
-            dodajBtn3.clicked.connect(self.dzialanie)
-            dodajBtn4.clicked.connect(self.dzialanie)
-
-           
-        self.resize(1200, 675)
         self.setWindowTitle("Menedżer nieoficjalnych dodatków")
-        self.show()
+        '''
+        url2 = globalURL+"img/tlo.jpg"
+        zdjecie = QLabel(self)
+        data =  urlopen(url2).read()
+        pixmap3 = QPixmap()
+        pixmap3.loadFromData(data)
+        zdjecie.setPixmap(pixmap3)
+       # layoutX.addWidget(zdjecie)
+       #layoutV.addChildLayout(layoutX)
+       '''
+        self.resize(1200, 675)
+        self.setFixedSize(1200,675)
+       
+        self.setStyleSheet("background-color: #007e43")
+        self.setLayout(layoutV)
 
+        #self.setLayout(layoutV)
+        self.show()
 
     def dzialanie(self):
 
@@ -105,4 +132,4 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     okno = menedzer()
-    sys.exit(app.exec_())
+sys.exit(app.exec_())
