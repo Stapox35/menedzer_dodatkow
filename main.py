6a0d48308d850f7baaeb2a0791a3650a9faebfaf
stapox35 <stapox35@gmail.com>
2019-07-02 14:09:07 +0200

    Dodana zakladka Kontakt, zostala tylko mechanika skryptu instalacji

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from urllib.request import urlopen
 import requests
 
 globalURL= "http://stapox.cal24.pl/"
-
+zmiennaCopyright = "Copyright © 2019 stapox "
 flagamiejsca = 0
 
 class menedzer(QWidget):
@@ -87,7 +87,7 @@ class menedzer(QWidget):
         
         layoutV.addLayout(layout2)
         napis2 = QLabel(self)
-        napis2.setText("Copyright © 2019 stapox ")
+        napis2.setText(zmiennaCopyright)
         napis2.setStyleSheet("font: 30pt Times New Roman; color: white; text-align: center; width: 1200px")
         layoutpomocniczy = QHBoxLayout()
         layoutpomocniczy.addWidget(napis2)
@@ -155,6 +155,10 @@ class menedzer(QWidget):
             #QMessageBox.warning(self, "Błąd", "Instalowanie dodatków", QMessageBox.Ok)
             self.clearLayout(layoutV)
             self.interfejs()
+        if nadawca.text() == "&Kontakt":
+            #QMessageBox.warning(self, "Błąd", "Instalowanie dodatków", QMessageBox.Ok)
+            self.clearLayout(layoutV)
+            self.funkcja3()
     
     def clearLayout(self, layout):
         if layout is not None:
@@ -240,7 +244,7 @@ class menedzer(QWidget):
         layoutV.addLayout(layout2)
 
         napis2 = QLabel(self)
-        napis2.setText("Copyright © 2019 stapox ")
+        napis2.setText(zmiennaCopyright)
         napis2.setStyleSheet("font: 25pt Times New Roman; color: white; text-align: center; width: 1200px; text-align: jutify")
         layoutpomocniczy = QHBoxLayout()
         layoutpomocniczy.addWidget(napis2)
@@ -323,12 +327,106 @@ class menedzer(QWidget):
         layoutV.addLayout(layout2)
 
         napis2 = QLabel(self)
-        napis2.setText("Copyright © 2019 stapox ")
+        napis2.setText(zmiennaCopyright)
         napis2.setStyleSheet("font: 25pt Times New Roman; color: white; text-align: center; width: 1200px; text-align: jutify")
         layoutpomocniczy = QHBoxLayout()
         layoutpomocniczy.addWidget(napis2)
         #layoutpomocniczy.addSpacing(50)
         layoutV.addLayout(layoutpomocniczy)
+
+    def funkcja3(self):
+        layout = QHBoxLayout()
+        dodajBtn = QPushButton("&Home", self)
+        dodajBtn.setStyleSheet("width: 100px; height: 75px;")
+        layout.addWidget(dodajBtn)
+        dodajBtn.clicked.connect(self.dzialanie)
+
+        label = QLabel(self)
+        url = globalURL+"img/logo_maszyna.gif"  
+        data = urlopen(url).read()
+        pixmap = QPixmap()
+        pixmap.loadFromData(data)
+        pixmap2 = pixmap.scaled(251, 70)
+        label.setPixmap(pixmap2)
+        label.move(20,15)
+        layout.addWidget(label)
+
+        napis = QLabel(self)
+        napis.setText("Menedżer nieoficjalnych dodatków")
+        napis.setStyleSheet("font: 30pt Times New Roman; color: white; font-weight: 700")
+        napis.move(400, 25)
+
+        layout.addWidget(napis)
+
+        layoutV.addLayout(layout)
+
+        layout2 = QHBoxLayout()
+
+        zdjecie = QLabel(self)
+        url2 = globalURL+"img/scr-kontakt.jpg"
+        data =  urlopen(url2).read()
+        pixmap3 = QPixmap()
+        pixmap3.loadFromData(data)
+        #  pixmap4 = pixmap3.scaled(755, 425)
+        zdjecie.setPixmap(pixmap3)
+        zdjecie.move(0,125)
+        
+        layout3 = QHBoxLayout()
+        layout3.setDirection(2)
+
+        napis5 = QLabel(self)
+        napis5.setText("Kontakt")
+        napis5.setStyleSheet("font: 20pt Times New Roman; color: white; font-weight: 700")
+        layout3.addWidget(napis5)
+
+        response = requests.get(globalURL+"files/config_menedzer_serwer.ini")
+        data = response.text
+        data = data.replace('\n', ' ')
+        i = 0
+        flagaoprojekcie = False
+        text = data.split(' ')
+        textpl = ""
+        for s in text:
+            if flagaoprojekcie:
+                if s == "[END]":
+                    flagaoprojekcie = False
+                    break
+                textpl = textpl+' '+s
+            if s == "[KONT]":
+                flagaoprojekcie = True
+                #print("rozpoczalem_wydzielac")
+        
+        #print(textpl[5:len(textpl)-2])
+
+        napisPL = QLabel(self)
+        setetx = textpl[5:len(textpl)-1]
+        setetx = setetx.split('"')
+        napisPL.setText(setetx[0])
+        napisPL.setStyleSheet("font: 16pt Times New Roman; color: white")
+        napisPL.setWordWrap(True)
+
+        napisPL2 = QLabel(self)
+        napisPL2.setText("Mail do uber-admina stapoxa: "+setetx[2])
+        napisPL2.setStyleSheet("font: 16pt Times New Roman; color: white")
+
+        layout3.addWidget(napisPL)
+        layout3.addWidget(napisPL2)
+        layout2.addLayout(layout3)
+        layout2.addWidget(zdjecie)
+        
+        layoutV.addLayout(layout2)
+
+        napis2 = QLabel(self)
+        napis2.setText(zmiennaCopyright)
+        napis2.setStyleSheet("font: 25pt Times New Roman; color: white; text-align: center; width: 1200px; text-align: jutify")
+        layoutpomocniczy = QHBoxLayout()
+        layoutpomocniczy.addWidget(napis2)
+        #layoutpomocniczy.addSpacing(50)
+        layoutV.addLayout(layoutpomocniczy)
+
+
+        
+
 if __name__ == '__main__':
     import sys
 
